0b33037dbc598854c8e398748c3924c3dd8ccf70
Vlad Ivanov <vlad.ivanov@hotmail.com>
2025-11-11 15:17:57 +0100

    basic REPL implementation

--- src/repl.ts
+++ src/repl.ts
@@ -1,7 +1,31 @@
+import { createInterface } from "node:readline";
+
 export function cleanInput(input: string): string[] {
   return input
     .toLowerCase()
     .trim()
     .split(" ")
     .filter((word) => word !== "");
+}
+
+export function startREPL(): void {
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
+
+  rl.prompt();
+
+  rl.on("line", async (input) => {
+    const words = cleanInput(input);
+    if (words.length === 0) {
+      rl.prompt();
+      return;
+    }
+
+    const commandName = words[0];
+    console.log(`Your command was: ${commandName}`);
+    rl.prompt();
+  });
 }
